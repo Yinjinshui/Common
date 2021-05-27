764161a4dce08e1be20349dc3e2635fad1d6f571
jinshui.yin <jinshui.yin@weicheche.cn>
2021-05-27 14:55:29 +0800

    first

---
+++ Cache/Redis.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Common\Cache;
+
+use Hyperf\Utils\ApplicationContext;
+
+/**
+ * redis二次封装
+ */
+class Redis
+{
+    private $redis;
+
+    public function __construct()
+    {
+        $container = ApplicationContext::getContainer();
+        $this->redis = $container->get(\Hyperf\Redis\Redis::class);
+    }
+
+    public function __call($name, $arguments)
+    {
+        return call_user_func_array([$this->redis, $name], $arguments);
+    }
+
+    /**
+     * 获取缓存
+     * @param string $key
+     * @return string|null
+     */
+    public function getArr(string $key)
+    {
+        $value = $this->redis->get($key);
+        if ($value) {
+            $value = unserialize($value);
+        }
+        return $value;
+    }
+
+    /**
+     * 设置缓存
+     * @param string $key
+     * @param string $value
+     * @return bool
+     */
+    public function setArr(string $key, array $value, $expire = 0)
+    {
+        $value = serialize($value);
+        $expire = intval($expire);
+        if ($expire === 0) {
+            $res = $this->redis->set($key, $value);
+        } else {
+            $res = $this->redis->set($key, $value, $expire);
+        }
+        return $res;
+    }
+}
